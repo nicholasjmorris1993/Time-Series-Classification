47b40b12ad420ccd03dd76451b8418f32cfc8f5d
Nick <129078463+nicholasjmorris1993@users.noreply.github.com>
2023-04-10 17:15:07 -0400

    Add files via upload

---
+++ test/test_deep_learning.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+import sys
+sys.path.append("/home/nick/Time-Series-Classification/src")
+from deep_learning import nnet
+
+
+data = pd.read_csv("/home/nick/Time-Series-Classification/test/traffic.txt", sep="\t")
+
+model = nnet(
+    df=data, 
+    datetime="Day", 
+    output="Vehicles", 
+    lags=7, 
+    forecasts=7, 
+    resolution=["day_of_week", "week_of_year"],
+    test_frac=0.5,
+    deep=False,
+)
+
+forecast = 7
+print(model.metric[model.output[forecast - 1]])
+
+predictions = model.predictions[model.output[forecast - 1]].copy()
+
+labels = np.unique(predictions.to_numpy())
+cmatrix = confusion_matrix(
+    y_true=predictions["Actual"],  # rows
+    y_pred=predictions["Predicted"],  # columns
+    labels=labels,
+)
+cmatrix = pd.DataFrame(cmatrix, columns=labels, index=labels)
+print(cmatrix)
+
+
